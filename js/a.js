38ab4d68bec98aa0cb3b71441d224cae68418fc5
Stupidpan <2812346909@qq.com>
2017-04-14 21:22:14 +0800

    a happy day

---
+++ js/a.js
@@ -0,0 +1,12 @@
+<script>   
+   function a(){
+     var audio = document.getElementById('music'); 
+     if(audio.paused){                 
+         audio.play();//audio.play();// 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+
+</script>
